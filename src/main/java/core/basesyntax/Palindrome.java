818c487ddfcfd462c9d05eea6ddbca1f578cb65e
Misha <mishabeheka@gmail.com>
2019-12-06 08:53:49 +0200

    DoneHW_4.3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class Palindrome {
 
     /**
-     * <p>Реализуйте метод, проверяющий, является ли заданная строка палиндромом. Палиндромом называется
+     * <p>Реализуйте метод, проверяющий, является ли заданная строка палиндромом.
+     * Палиндромом называется
      * строка, которая читается одинаково слева направо и справа налево (в том числе пустая).
      * При определении "палиндромности" строки должны учитываться только буквы и цифры. А пробелы,
      * знаки препинания, а также регистр символов должны игнорироваться.</p>
@@ -20,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.replaceAll("[^A-z0-9]+", "").trim();
+        StringBuilder builder = new StringBuilder(text);
+        return builder.reverse().toString().equalsIgnoreCase(text);
     }
 }
